42c1d90fea3b3d1b5b127708212c00b61c92afca
Jenny Robbins <Jenny.Robbins@softwire.com>
2022-10-31 14:21:59 +0000

    added all books to controller

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,13 +1,21 @@
 import { Router, Request, Response } from 'express';
+import {User, Book, CheckedOut} from '../sequelizeDefines'
 
 class BookController {
     router: Router;
 
     constructor() {
         this.router = Router();
-        this.router.get('/:id', this.getBook.bind(this));
+        this.router.get('/search/:id', this.getBook.bind(this));
 
         this.router.post('/', this.createBook.bind(this));
+        this.router.get('/allBooks', this.getBooksAsList.bind(this))
+    }
+
+    async getBooksAsList(req: Request, res: Response){
+        const books = await Book.findAll();
+        return res.status(500).json({books,
+        });
     }
 
     getBook(req: Request, res: Response) {
